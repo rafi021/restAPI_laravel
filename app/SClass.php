f87ad2ce24d3aa9448edbd1e0626ee6e9179f6d9
rafi021 <mahmud.ibrahim021@gmail.com>
2020-08-09 05:20:23 +0600

    Create a SClass model, migration, factory and Seeder

---
+++ app/SClass.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SClass extends Model
+{
+    protected $fillable = [
+        'class_name'
+    ];
+
+}
---
+++ database/factories/SClassFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\SClass;
+use Faker\Generator as Faker;
+
+$factory->define(SClass::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
